6d166f0e9a74248fc1274b363fc074989871f911
Rabbi-Morgan <65710152+Rabbi-Morgan@users.noreply.github.com>
2021-10-26 00:24:46 +0100

    my first core7 commit

---
+++ core7.js
@@ -0,0 +1,69 @@
+const toggleBtn = document.getElementById('menu_open');
+const menu = document.querySelector('.menu');
+const loader = document.querySelector('.loader--loading');
+const video = document.querySelector('.hero_video');
+const body = document.querySelector('body');
+const arrow = document.querySelector('.arrow');
+const about_cont = document.querySelector('.section1');
+const about_cot = document.querySelector('.about');
+const about = document.querySelectorAll('.about_img');
+const topPos = about_cot.getBoundingClientRect().top + window.scrollY;
+const bottomPos = about_cont.getBoundingClientRect().bottom + window.scrollY;
+const scrollReview = document.querySelector('.review_cont');
+const leftReview = document.getElementById('prev');
+const rightReview = document.getElementById('next');
+const navItems = document.querySelectorAll('.nav_list li a')
+
+toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('open');
+    toggleBtn.classList.toggle('show');
+    body.classList.toggle('no--scroll');
+})
+navItems.forEach(nav => {
+    nav.addEventListener('click', ()=>{
+        menu.classList.remove('open');
+    toggleBtn.classList.remove('show');
+    body.classList.remove('no--scroll');
+    })
+});
+// to start the animation when the video is loaded
+video.addEventListener('loadeddata', ()=> {
+    loader.style.animation = `loading ${video.duration}s linear infinite`;
+});
+
+// make the arrow visible after 560 viewport height
+window.addEventListener('scroll', ()=> {
+    if(this.scrollY >= 560) arrow.classList.add('show--arrow'); else arrow.classList.remove('show--arrow');
+    // about transition on scroll
+    about.forEach(imgs => {
+        if(screen.width <= 500){
+            if(this.scrollY >= bottomPos){
+            imgs.classList.add('scale');}
+        }else {
+            if(this.scrollY >= bottomPos){
+            imgs.classList.add('scale');}
+        }
+    })   
+});
+// for the carousel 
+
+rightReview.addEventListener('click', ()=> {
+    scrollReview.scrollLeft += 290;
+    check();    
+});
+leftReview.addEventListener('click', ()=> {
+    scrollReview.scrollLeft -= 287;    
+    check();
+});
+const maxScrollLeft = scrollReview.scrollWidth - scrollReview.clientWidth;
+const prevH = document.querySelector('.prev');
+const nextH = document.querySelector('.next');
+function check() {
+    if (scrollReview.scrollLeft > (maxScrollLeft - 1)) {
+        nextH.classList.remove('move');
+        prevH.classList.add('move');
+    }else {
+       nextH.classList.add('move');
+        prevH.classList.remove('move');
+    }
+}
